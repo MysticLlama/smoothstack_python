513f800bd957dcfd91b51d0ad24a821454d4fa8c
MysticLlama <joel.joshi@gmail.com>
2022-02-07 23:54:08 -0500

    Update ex2.py
    comments pass

--- ex2.py
+++ ex2.py
@@ -8,6 +8,7 @@ def one():
 def two():
 	print("Example 2)\n")
 	print(6^6)
+	#print(30+*6) # this is not valid syntax
 	print(6**6)
 	print(6+6+6+6+6+6)
 	print()
@@ -23,36 +24,40 @@ def three():
 	
 
 	
-	
+#used to help check if payment amount "a" satisfies the mortgage given "p" principal, "r" anual rate, "l" length of payment.
+#returns touple of (number payments of "a" required to pay off mortgage, remaining payment)  
 def test(p,r,a,l):
-	sum =p
-	count =0
+	sum =p #keeps track of working sum of payment left
+	count =0 #tracks number of paymenst required
 	while(sum >0):
-		sum *= 1+((r/12)/100)
-		sum -=a
-		count +=1
+		sum *= 1+((r/12)/100) ##apply interest for the month
+		sum -=a #subtract the amount we're trying
+		count +=1 #++ to mark down this month
 		
-		if(count > l):
-			return(l+1,p)
+		if(count > l): #if the number of payments needed exceeds the length we're given, we can stop early with a bad value that auto-fails
+			return(l+1,p*l) #return a pair of numbers that could not possibly work to mark this try as irrelevant
 	
-	return (count,sum)
+	return (count,sum) #return as touple for debugging
 	
-	
-def four(p,r,l):
-	m = 10000
-	t =test(p,r,m,l)
-	smol = m
-	sol = 0
-	while( t  !=(l,0)):
-		m -=1
-		if(abs(t[1]) < smol):
-			smol =abs(t[1])
-			sol = m+1	
+
+
+#find a constant monthly payment that best pays off the mortgage with "p" principal, "r" anual rate increase, "l" length of payment
+#such that the monthly payment is under 10,000 per month.	
+def four(p,r,l): 
+	m = 10000 #spec defines 10,000 as the maximum possible monthly payment.
+	t =test(p,r,m,l) #used to track current iteration of test
+	smol = m #value used to track smallest value
+	sol = 0 #solution value, define as 0, for invalid payments should we not find a valid value for monthly payment
+	while( True):
+		m -=1 #check every amount from 10,000 down to 0 until we find a good value
+		if(abs(t[1]) < smol): #keep track of the smallest end-of-period difference to find best value
+			smol =abs(t[1]) #track absolute value
+			sol = m+1 #add 1 to m because of indexing order.	
 		
 		if(m<1):
-			break
-		t = test(p,r,m,l)
-	print(sol)
+			break #stop looking if m would be an invalid payment.
+		t = test(p,r,m,l) #set next m to pay
+	print(sol) #"return" our value via print
 		
 	
 	
